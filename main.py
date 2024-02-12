43e1739eb2b44f073772e226c9097a71072dc45a
James Campbell <james.e.campbell@outlook.com>
2024-02-12 08:54:34 +0000

    Testing echo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import src.app.calculator as calculator
+# from osgeo import ogr
 
 
 if __name__ == "__main__":
     print("Hellow shapefile processor.")
     print("One plus one equals: ", calculator.add(1, 1))
+
+
