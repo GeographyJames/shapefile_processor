db882dc2666dd23f911f7450e38da1d688b821fe
James Campbell <james.e.campbell@outlook.com>
2024-02-14 07:02:39 +0000

    Added a fiona test.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import src.app.calculator as calculator
 from src.app import shapefile_processor
 
 
 if __name__ == "__main__":
     print("Hellow shapefile processor.")
     shapefile_processor.test()
-
-
--- src/app/shapefile_processor.py
+++ src/app/shapefile_processor.py
@@ -6,3 +6,8 @@ def test():
     path = Path("tests/test_data/1_valid_polygon_OSGB36.shp")
     with fiona.open(path) as f:
         print(f.crs)
+
+def open_shapefile(path: Path) -> fiona.collection:
+    with fiona.open(path) as f:
+        return f
+        
