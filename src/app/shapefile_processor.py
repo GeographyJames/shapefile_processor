77eed78fb0b4df0f03195328ed6e6ae2e754c336
James Campbell <james.e.campbell@outlook.com>
2024-02-14 14:34:10 +0000

    Added a function to return crs.

--- src/app/shapefile_processor.py
+++ src/app/shapefile_processor.py
@@ -1,13 +1,17 @@
 import fiona
 from pathlib import Path
+from fiona.crs import CRS
 
-
-def test():
-    path = Path("tests/test_data/1_valid_polygon_OSGB36.shp")
-    with fiona.open(path) as f:
-        print(f.crs)
+path = Path("tests/test_data/1_valid_polygon_OSGB36.shp")
 
 def open_shapefile(path: Path) -> fiona.collection:
     with fiona.open(path) as f:
         return f
         
+def crs(path: Path) -> str:
+    with fiona.open(path) as f:
+        return CRS.to_string(f.crs)
+    
+if __name__ == "__main__":
+    print(type(crs(path)))
+    print(crs(path))
